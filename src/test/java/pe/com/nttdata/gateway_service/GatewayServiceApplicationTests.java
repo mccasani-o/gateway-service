36879c65c151894c973d4c23e7aab2f4553b6590
Mauricio Cc <mauricioccasani50@gmail.com>
2024-11-21 17:48:07 -0500

    gateway-service

---
+++ src/test/java/pe/com/nttdata/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.nttdata.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
